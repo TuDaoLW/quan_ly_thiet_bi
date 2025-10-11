ec487fa3386f8aa793dea5f81be10bbbda76fdfb
tudaolw <daylatu951@gmail.com>
2025-10-11 09:24:08 +0700

    initial for project

---
+++ qlytbi/src/main/java/com/qlph/qlytbi/QlytbiApplication.java
@@ -0,0 +1,13 @@
+package com.qlph.qlytbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlytbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlytbiApplication.class, args);
+	}
+
+}
---
+++ qlytbi/src/test/java/com/qlph/qlytbi/QlytbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlph.qlytbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlytbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
